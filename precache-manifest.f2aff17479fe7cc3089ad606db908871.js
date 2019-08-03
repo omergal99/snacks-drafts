ece57c12d4a065ffc37271ae54bd638134e847c5
omergal99 <omergal99gm@gmail.com>
2019-08-03 19:19:05 +0300

    Updates

---
+++ precache-manifest.f2aff17479fe7cc3089ad606db908871.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be8b63a2fc533c0fb454528f0501cd07",
+    "url": "/snacks-drafts/index.html"
+  },
+  {
+    "revision": "87cc6cd2b96ad4ecd2d1",
+    "url": "/snacks-drafts/static/css/main.81f58d40.chunk.css"
+  },
+  {
+    "revision": "536819451b114efee170",
+    "url": "/snacks-drafts/static/js/2.01b91702.chunk.js"
+  },
+  {
+    "revision": "87cc6cd2b96ad4ecd2d1",
+    "url": "/snacks-drafts/static/js/main.4aa80775.chunk.js"
+  },
+  {
+    "revision": "be2487767616029210a4",
+    "url": "/snacks-drafts/static/js/runtime~main.43233a0b.js"
+  },
+  {
+    "revision": "ca4550ad2edcc95c1086266f83616636",
+    "url": "/snacks-drafts/static/media/Amiri-Regular.ca4550ad.ttf"
+  },
+  {
+    "revision": "36276bf672f94c08cc5a57966cb49641",
+    "url": "/snacks-drafts/static/media/Cardo-RegularItalic.36276bf6.ttf"
+  },
+  {
+    "revision": "306f2307137eff1e9f65be4e63ebe888",
+    "url": "/snacks-drafts/static/media/IMFellDoublePica-RegularItalic.306f2307.ttf"
+  },
+  {
+    "revision": "56cb366910aa712773f8f2f7e7f42b9f",
+    "url": "/snacks-drafts/static/media/Merienda-Regular.56cb3669.ttf"
+  },
+  {
+    "revision": "cfb6f28612cdb09ea2aa33e0eb30ef92",
+    "url": "/snacks-drafts/static/media/OldStandardTT-RegularItalic.cfb6f286.ttf"
+  },
+  {
+    "revision": "849e67957ea4dd5edb38eedb9d5743f1",
+    "url": "/snacks-drafts/static/media/Quando-Regular.849e6795.ttf"
+  },
+  {
+    "revision": "eeb5d6d85d2a7ab7537c9eed68975768",
+    "url": "/snacks-drafts/static/media/man hole.eeb5d6d8.png"
+  },
+  {
+    "revision": "f9769f2eaf4fd029614796c440e7ff22",
+    "url": "/snacks-drafts/static/media/new-zealand-1.f9769f2e.jpg"
+  },
+  {
+    "revision": "32ac2bf62bef11e165ba8214413e6f3e",
+    "url": "/snacks-drafts/static/media/new-zealand-2.32ac2bf6.jpg"
+  },
+  {
+    "revision": "89ae2232ea169e2514ea874ecf714dc1",
+    "url": "/snacks-drafts/static/media/new-zealand-3.89ae2232.jpg"
+  },
+  {
+    "revision": "e7863c7ba9670a3d2d987985032c48fa",
+    "url": "/snacks-drafts/static/media/new-zealand-4.e7863c7b.jpg"
+  }
+]);
